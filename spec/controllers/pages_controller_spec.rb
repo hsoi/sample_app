0fddfad5d93e82e4e784ff9129101d288fa54d8c
John C. Daub <hsoi@hsoienterprises.com>
2012-09-06 11:59:49 -0500

    adding the erb for the static-ish pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,6 +10,12 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right titie" do
+        get 'home'
+        response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+    
   end
 
   describe "GET 'contact'" do
@@ -17,6 +23,12 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right titie" do
+        get 'contact'
+        response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+
   end
 
   describe "GET 'about'" do
@@ -24,5 +36,10 @@ describe PagesController do
         get 'about'
         response.should be_success
      end 
+
+     it "should have the right titie" do
+         get 'about'
+         response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+     end
   end
 end
